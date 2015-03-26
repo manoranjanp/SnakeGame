234f9fb7ccf7d335f985af325456a13e7cbc1349
manoranjanp <manoranjan.113@gmail.com>
2015-03-26 22:46:43 +0530

    v1.0 Added

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manoranjan_Padhy
+ */
+public class GUI {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Manoranjan_Padhy
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Snake();
+    }
+}
